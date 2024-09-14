32931c545aaebd9057607a4a3222f611aa969b54
Benjamin <benjamin.sokolov19@gmail.com>
2024-09-14 22:41:29 +0300

    add: home, constructor, feed routers

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,18 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, NotFound404 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='*' element={<NotFound404 />}/>
+      <Route path='/' element={<AppHeader />} />
+      <Route path='/constructor-page' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />}/>
+    </Routes>
   </div>
 );
 
