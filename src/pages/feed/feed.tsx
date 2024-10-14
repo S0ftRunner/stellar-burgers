ae2780315383d1d2e4dac0e62918f24f6f4cb7fd
Benjamin <benjamin.sokolov19@gmail.com>
2024-10-14 21:25:20 +0300

    fix: preloader add in feed info

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -3,7 +3,7 @@ import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
 import { FC, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { getFeeds, getFeedsSelector } from '../../services/slices/feedsSlice';
+import { getFeeds, getFeedsSelector, getIsLoadingOrderSelector } from '../../services/slices/feedsSlice';
 import { useDispatch } from '../../services/store';
 
 // TODO: прописать обновление заказов
@@ -14,14 +14,21 @@ export const Feed: FC = () => {
     dispatch(getFeeds());
   }, [dispatch]);
   const orders: TOrder[] = useSelector(getFeedsSelector);
+  const isLoading = useSelector(getIsLoadingOrderSelector);
   const handleGetFeeds = () => {
     dispatch(getFeeds());
     console.log('произошло обновление списка');
   }
 
+  if (isLoading) {
+    return <Preloader />
+  }
+  
   if (!orders.length) {
     return <Preloader />;
   }
+
+
 
   return <FeedUI orders={orders} handleGetFeeds={handleGetFeeds} />;
 };
