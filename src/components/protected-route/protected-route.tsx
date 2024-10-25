b1bc261901f563ad9020faee705700948a392908
Benjamin <benjamin.sokolov19@gmail.com>
2024-10-25 11:02:40 +0300

    finish: second iteration

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -5,15 +5,21 @@ import { Preloader } from "@ui";
 
 type TProtectedRouteProps = {
   children: React.ReactElement;
+  anonymous?: boolean;
 }
 
-export const ProtectedRoute = ({children}: TProtectedRouteProps) => {
+export const ProtectedRoute = ({children, anonymous = false}: TProtectedRouteProps) => {
 
-  const {data, isAuthChecked} = useSelector(getUserDataSelector);
+  const {data, isAuthChecked, isAuth} = useSelector(getUserDataSelector);
   const location = useLocation();
+  const from = location.state?.from || '/';
 
   if (!isAuthChecked) {
     return <Preloader />
+  }
+
+  if (anonymous && isAuth) {
+    return <Navigate to={ from } />;
   }
 
   if (!data.email || !data.name) {
