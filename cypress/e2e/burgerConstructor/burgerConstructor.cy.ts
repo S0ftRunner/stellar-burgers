aebd98ff80708d865a4ce56a81680bb13415d2d5
Benjamin <benjamin.sokolov19@gmail.com>
2024-12-01 21:21:58 +0300

    add: mock data for e2e testing

--- cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
+++ cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
@@ -1,5 +1,14 @@
-describe('Проверяем доступность приложения',  () => {
-  it('Приложение по адресу localhost:4000 - доступно:', () => {
-    cy.visit('http://localhost:4000');
+
+
+describe('Проверяем доступность приложения', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients' });
+    cy.visit('http://localhost:4000/');
+  });
+
+
+  it('Ингредиенты доступны для выбора', () => {
+    cy.get('[data-ingredient="bun"]').should('have.length.at.least', 1);
   })
-})
+
+});
