1c95a065c7c8e14869b81b9eb0456fc46e2138e5
Benjamin <benjamin.sokolov19@gmail.com>
2024-12-12 21:41:31 +0300

    add: test userOrderReducer

--- __tests__/userOrderReducer.test.ts
+++ __tests__/userOrderReducer.test.ts
@@ -1,26 +1,35 @@
-import { userOrderReducer, createOrder, initialState } from "../src/services/slices/userOrderSlice";
-import { resetOrderModalData, getFeeds } from "../src/services/slices/feedsSlice";
+import {
+  userOrderReducer,
+  createOrder,
+  initialState
+} from '../src/services/slices/userOrderSlice';
+import {
+  resetOrderModalData,
+  getFeeds
+} from '../src/services/slices/feedsSlice';
 
 const orderMockData = [
   {
     ingredients: [
       '643d69a5c3f7b9001cfa093c',
       '643d69a5c3f7b9001cfa093e',
-      '643d69a5c3f7b9001cfa093c',
+      '643d69a5c3f7b9001cfa093c'
     ],
     _id: '66223473h74df0231c0462b3',
     status: 'done',
     name: 'TEST_NAME',
     createdAt: '2024-10-19T09:10:21.123Z',
     updatedAt: '2024-10-19T09:10:24.123Z',
-    number: 43223,
+    number: 43223
   }
-]
+];
 
 describe('тест userOrderReducer', () => {
   test('Начало запроса', () => {
-    const initState = userOrderReducer(initialState, getFeeds.pending('pending'));
+    const initState = userOrderReducer(
+      initialState,
+      createOrder.pending('pending', orderMockData[0].ingredients)
+    );
     expect(initState.orderRequest).toBeTruthy();
-  })
-
-})
+  });
+});
