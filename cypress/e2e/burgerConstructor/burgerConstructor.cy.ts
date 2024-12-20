7aa0ed8453d39203612abdc8ee56d68e34c31f6e
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-12-20 17:49:47 +0300

    add: реализация тестирования e2e

--- cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
+++ cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
@@ -1,14 +1,34 @@
-
+import * as orderFixture from '../../fixtures/order.json';
 
 describe('Проверяем доступность приложения', () => {
   beforeEach(() => {
-    cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients' });
-    cy.visit('http://localhost:4000/');
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
+    cy.visit('/');
   });
-
 
   it('Ингредиенты доступны для выбора', () => {
     cy.get('[data-ingredient="bun"]').should('have.length.at.least', 1);
-  })
+    cy.get('[data-ingredient="bun"], ["data-ingredient="sauce"]').should(
+      'have.length.at.least',
+      1
+    );
+  });
 
+  describe('Проверка работы модальных окон', () => {
+    describe('Открытие модального окна', () => {
+      it('Открытие карточки ингредиента', () => {
+        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get('#modals').children().should('have.length', 2);
+      });
+    });
+
+    describe('Проверка закрытия модальных окон', () => {
+      it('Нажатие на крестик: ', () => {
+        cy.get('[data-ingredients="bun"]:first-of-type').click();
+        cy.get('#modals button:first-of-type').click();
+        cy.wait(500);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+    });
+  });
 });
