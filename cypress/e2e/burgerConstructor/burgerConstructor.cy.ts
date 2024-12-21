b658a5499335432385810e35ae760111644e232f
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-12-21 12:40:43 +0300

    add: доделал для ревью

--- cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
+++ cypress/e2e/burgerConstructor/burgerConstructor.cy.ts
@@ -1,34 +1,96 @@
 import * as orderFixture from '../../fixtures/order.json';
 
-describe('Проверяем доступность приложения', () => {
+describe('Тест конструктора', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
     cy.visit('/');
   });
 
-  it('Ингредиенты доступны для выбора', () => {
+  it('Список ингредиентов доступен для выбора', () => {
     cy.get('[data-ingredient="bun"]').should('have.length.at.least', 1);
-    cy.get('[data-ingredient="bun"], ["data-ingredient="sauce"]').should(
+    cy.get('[data-ingredient="main"],[data-ingredient="sauce"]').should(
       'have.length.at.least',
       1
     );
   });
 
-  describe('Проверка работы модальных окон', () => {
-    describe('Открытие модального окна', () => {
-      it('Открытие карточки ингредиента', () => {
+  describe('Проверка работы модальных окон описаний ингредиентов', () => {
+    describe('Проверка открытия модальных окон', () => {
+      it('Базовое открытие по карточке ингредиента', () => {
         cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get('#modals').children().should('have.length', 2);
+      });
+
+      it('Модальное окно с ингредиентом будет открыто после перезагрузки страницы', () => {
+        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.reload(true);
         cy.get('#modals').children().should('have.length', 2);
       });
     });
 
     describe('Проверка закрытия модальных окон', () => {
-      it('Нажатие на крестик: ', () => {
-        cy.get('[data-ingredients="bun"]:first-of-type').click();
+      it('Через нажатие на крестик', () => {
+        cy.get('[data-ingredient="bun"]:first-of-type').click();
         cy.get('#modals button:first-of-type').click();
         cy.wait(500);
         cy.get('#modals').children().should('have.length', 0);
       });
+
+      it('Через нажатие на оверлей', () => {
+        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+        cy.wait(500);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+
+      it('Через нажатие на Escape', () => {
+        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get('body').type('{esc}');
+        cy.wait(500);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+    });
+  });
+
+  describe('Процесс оформления заказа', () => {
+    beforeEach(() => {
+      cy.setCookie('accessToken', 'EXAMPLE_ACCESS_TOKEN');
+      localStorage.setItem('refreshToken', 'EXAMPLE_REFRESH_TOKEN');
+
+      cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+      cy.intercept('POST', 'api/orders', { fixture: 'order.json' });
+      cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+
+      cy.visit('/');
+    });
+
+    it('Базовая процедура оформления ПОСЛЕ авторизации', () => {
+      // Проверка работы конструктора, по умолчанию он отключен пока не будет выбрана хотя бы 1 ингредиент и булка
+      cy.get('[data-order-button]').should('be.disabled');
+      cy.get('[data-ingredient="bun"]:first-of-type button').click();
+      cy.get('[data-order-button]').should('be.disabled');
+      cy.get('[data-ingredient="main"]:first-of-type button').click();
+      cy.get('[data-order-button]').should('be.enabled');
+
+      // Нажатие на саму кнопку оформления заказа
+      cy.get('[data-order-button]').click();
+
+      // После успешной отправки данных на сервер должно быть открыто модальное окно с оформлением заказа
+      cy.get('#modals').children().should('have.length', 2);
+
+      // Новое модальное окно должно содержать тестовый номер заказа
+      cy.get('#modals h2:first-of-type').should(
+        'have.text',
+        orderFixture.order.number
+      );
+
+      cy.get('[data-order-button]').should('be.disabled');
+    });
+
+    afterEach(() => {
+      // Очистка токенов
+      cy.clearCookie('accessToken');
+      localStorage.removeItem('refreshToken');
     });
   });
 });
